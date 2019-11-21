62b62728568195ba98d2c3e0c1008889840cbfe8
dineshkumar7777 <54579541+dineshkumar7777@users.noreply.github.com>
2019-11-21 15:48:16 +0530

    armstrong program

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+def dinesh(p):
+  sum=0
+  temp=p
+  while temp >0:
+    digit =temp % 10
+    sum = digit**3
+    temp //=10
+  if p==sum:
+    print(p," it is armstrong number")   
+  else:
+    print(p,"it is not an armstrong number")
+dinesh(153)
